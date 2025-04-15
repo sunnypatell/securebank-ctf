db4c1ef744ec5fd954a674cc91d72f122c8ef2f7
sunnypatell <sunnypatel124555@gmail.com>
2025-04-15 19:12:36 -0400

    chore(site favicon): same as landing logo

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -8,6 +8,12 @@ const inter = Inter({ subsets: ["latin"] })
 export const metadata: Metadata = {
   title: "SecureBank",
   description: "Banking application - CTF Final Project - CSCI 3540U",
+  generator: 'www.sunnypatel.net',
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon.ico',
+    apple: '/favicon.ico',
+  },
 }
 
 export default function RootLayout({
